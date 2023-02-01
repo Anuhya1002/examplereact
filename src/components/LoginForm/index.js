4e29b5567ebf1edb3680bcc14b4d468e711de9e2
Anuhya@1002 <anuhya27@gmail.com>
2023-02-01 19:19:12 +0530

    login page edit

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,12 @@
+class LoginForm extends Component {
+  render() {
+    return (
+      <div>
+        <button>Login</button>
+        <p>KJVKJDSBVKB</p>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
